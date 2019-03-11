6078e24b4db471b99b765757936a6980d02d1613
itsvrushabh <itsvrushabh@gmail.com>
2019-03-11 12:10:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="python-patterns",
+    description="A collection of design patterns and idioms in Python.",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
